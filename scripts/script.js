b3f91e9d80de5d17fb0eafbd382e3c3ea776482f
Broodco <broodcoorens.maxime@gmail.com>
2019-02-04 15:06:33 +0100

    Collision implemented - Creation of start screen and end screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,56 @@
+// Starts the game
+// Creates the player shaceship and the first target
+// Also initialize shotexist, numshot et shotsfired, variables needed to create shots
+
+function startGame(){
+    document.querySelector(".splashScreen").style.display = "none";
+    document.querySelector(".area").style.display = "block";
+    gameArea.createCanvas();
+    gameArea.start();
+    shotexist = false;
+    targetsCount = 0;
+    numshot = 0;
+    shotsfired = [];
+    reload = 50;
+    spaceship = new drawcomp(document.querySelector("#spaceship"),300,450,200,200);
+    alien = new drawcomp(document.querySelector('#alien'),getRandomPosition()[0],getRandomPosition()[1],120,120);
+}
+
+function endGame(){
+
+}
+
+// Create the canvas into the html body
+// -- start : initialize the canvas and sets its attributes
+// -- start : also includes evenListeners changing the "key" property of the gameArea based on the key pressed by user.
+// -- clear : clear everything from the canvas, used to prevent duplicates when refreshing the canvas
+
+var gameArea = {
+    canvas : document.createElement("canvas"),
+    createCanvas : function(){
+        this.canvas.height = 600;
+        this.canvas.width = 800;
+        this.canvas.style.background = "black";
+        this.context = this.canvas.getContext("2d");
+        document.querySelector(".area").insertBefore(this.canvas,document.querySelector(".area").childNodes[0]);},
+    start : function(){
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function(e){
+            gameArea.keys = (gameArea.keys || []);
+            gameArea.keys[e.keyCode] = true;
+        })
+        window.addEventListener('keyup',function(e){
+            gameArea.keys[e.keyCode] = false;
+        })
+    },
+    clear : function() {
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+      }
+}
+
 // Create a random position within the canvas
 // This is used to create a position for the alien
 
@@ -10,47 +63,6 @@ function getRandomPosition() {
     return [randomX,randomY];
 }
 
-// Starts the game
-// Creates the player shaceship and the first target
-// Also initialize shotexist, numshot et shotsfired, variables needed to create shots
-
-function startGame(){
-    gameArea.start();
-    shotexist = false;
-    numshot = 0;
-    shotsfired = [];
-    reload = 50;
-    spaceship = new drawcomp(document.querySelector("#spaceship"),100,450,200,200);
-    alien = new drawcomp(document.querySelector('#alien'),getRandomPosition()[0],getRandomPosition()[1],120,120);
-}
-
-// Create the canvas into the html body
-// -- start : initialize the canvas and sets its attributes
-// -- start : also includes evenListeners changing the "key" property of the gameArea based on the key pressed by user.
-// -- clear : clear everything from the canvas, used to prevent duplicates when refreshing the canvas
-
-var gameArea = {
-    canvas : document.createElement("canvas"),
-    start : function(){
-        this.canvas.height = 600;
-        this.canvas.width = 800;
-        this.canvas.style.background = "black";
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('keydown', function(e){
-            gameArea.keys = (gameArea.keys || []);
-            gameArea.keys[e.keyCode] = true;
-        })
-        window.addEventListener('keyup',function(e){
-            gameArea.keys[e.keyCode] = false;
-        })
-    },
-    clear : function() {
-        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
-    }
-}
-
 // Create an image into the canvas
 // --update is used to redraw the image every 20ms
 // --newPos is used to refresh coordinates and change those if needed
@@ -60,7 +72,6 @@ function drawcomp(src,x,y,width,height){
     this.x = x;
     this.y = y;
     this.speedX = 0;
-    this.speedY = 0;
     this.width = width;
     this.height= height;
     this.update = function(){
@@ -69,12 +80,25 @@ function drawcomp(src,x,y,width,height){
     }
     this.newPos = function(){
         this.x += this.speedX;
-        this.y += this.speedY;
+    }
+    this.collision = function(laserObj){
+        var a_left = this.x;
+        var a_right = this.x + (this.width);
+        var a_bottom = this.y + (this.height);
+        var las_center = laserObj.x + 100;
+        var las_top = laserObj.y;
+        if ((a_bottom>=las_top) && (a_left <= las_center) && (a_right >= las_center)
+        ){
+            crash = true;
+        }else{
+            crash = false;
+        }
+        return crash;
     }
 }
 
 // Draw the laser shots image onto the canvas
-// Different from the other because it takes no argument and we use the prototype to declare the newpos and update functions. This enable us to create multiple instance of the same object onto the canvas.
+// Different from the other because it takes no argument and we use the prototype to declare the newpos and update functions. This enables us to create multiple instances of the same object onto the canvas.
 
 function drawshots(){
     this.src = document.querySelector('#shots');
@@ -102,13 +126,16 @@ function updateGameArea(){
     gameArea.clear();
     spaceship.speedX = 0;
     reload--;
-    if (gameArea.keys && gameArea.keys[37]){spaceship.speedX = -4}
-    if (gameArea.keys && gameArea.keys[39]){spaceship.speedX = 4;}
+    if (targetsCount == 10){
+        gameArea.stop();
+        endGame();
+    }
+    if (gameArea.keys && gameArea.keys[37]){spaceship.speedX = -6}
+    if (gameArea.keys && gameArea.keys[39]){spaceship.speedX = 6;}
     // Next block creates a laser shot if the reload time is under 0
     // Each shot is part of an array named shotsfired, this way we can have multiple shots on screen at the same time
     if (gameArea.keys && gameArea.keys[32] && reload <= 0){
-        shotsfired[numshot] = new drawshots();
-        numshot++;
+        shotsfired = new drawshots();
         shotexist = true;
         reload = 50;
     }
@@ -117,9 +144,22 @@ function updateGameArea(){
     alien.update();
     // Updates the different shots only if there is one
     if (shotexist == true){
-        for (i = 0 ; i< shotsfired.length; i++){
-        shotsfired[i].newPos();
-        shotsfired[i].update();
+        shotsfired.newPos();
+        shotsfired.update();
+        if ((shotsfired.y) < 0){
+            shotsfired = 0;
+            shotexist = false;
+            console.log("out")
+        }
+        // Checks if laser shots and the target are in the same place. If so, deletes both, specify that there's no current shot and recreates a target at a random position.
+        else if (alien.collision(shotsfired)==true){
+            targetsCount += 1;
+            alien = 0;
+            alien.x = getRandomPosition()[0];
+            alien.y = getRandomPosition()[1];
+            shotsfired = 0;
+            shotexist = false;
+            alien = new drawcomp(document.querySelector('#alien'),getRandomPosition()[0],getRandomPosition()[1],120,120);
         }
     }
 }
